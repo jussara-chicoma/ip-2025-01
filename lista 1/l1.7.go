98f90a5b1a48464dbf4fb47915ac8a3bc556e56e
jussara-chicoma <chicoma.borges@discente.ufg.br>
2026-03-25 16:18:14 -0300

    Update l1.7.go

--- lista 1/l1.7.go
+++ lista 1/l1.7.go
@@ -6,6 +6,6 @@ func main(){
 	fmt.Scan("polegadas")
 	c:= (5*f - 160)/ 9
 	mm:= polegadas*25.4
-	fmt.Printf ( "o valor em cesius = %.2f n\,"c)
+	fmt.Printf ( "o valor em cesius = %.2f n\,""c)
 	fmt.Printf(" A quantidade de chuva e =%.2f n\,"mm)
 }
