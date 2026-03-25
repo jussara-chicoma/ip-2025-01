0c2c3479d5db4f9400a40a41d70ec8ce41040ef0
jussara-chicoma <chicoma.borges@discente.ufg.br>
2026-03-25 15:56:07 -0300

    Add files via upload

---
+++ lista 1/l1.7.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+func main(){
+	var f,polegadas float64
+	fmt.Scan(&f)
+	fmt.Scan("polegadas")
+	c:= (5*f - 160)/ 9
+	mm:= polegadas*25.4
+	fmt.Printf ( "o valor em cesius = %.2f n\,"c)
+	fmt.Printf(" A quantidade de chuva e =%.2f n\,"mm)
+}
