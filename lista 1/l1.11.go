7a0bf3056b8ddf5175ee0fa1d53ae6f157e3ec79
jussara-chicoma <chicoma.borges@discente.ufg.br>
2026-03-27 00:52:30 -0300

    Add files via upload

---
+++ lista 1/l1.11.go
@@ -0,0 +1,14 @@
+package main
+import(
+	"fmt"
+
+)
+func main (){
+	var num int
+fmt.Scan(&num)
+if num%3== 0 && num%5 == 0 
+fmt.Println("o numero é divisivel")
+
+}else{
+	fmt.Println("O número não é divisivel")
+}
