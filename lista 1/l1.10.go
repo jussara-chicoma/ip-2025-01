216f91a0ff28f6c6d3c17933e4e4fc75cc4a7f32
jussara-chicoma <chicoma.borges@discente.ufg.br>
2026-03-27 00:51:39 -0300

    Add files via upload

---
+++ lista 1/l1.10.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a, b, c, d float64
+
+	fmt.Scan(&a)
+	fmt.Scan(&b)
+	fmt.Scan(&c)
+	fmt.Scan(&d)
+
+	// Cálcular  determinante: ad - bc
+	determinante := a*d - b*c
+
+	fmt.Printf("O VALOR DO DETERMINANTE E = %.2f\n", determinante)
+}
